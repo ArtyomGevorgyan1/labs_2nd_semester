b420ab51cf391d89b573421e986966a3e14cd745
ArtyomGevorgyan1 <43173333+ArtyomGevorgyan1@users.noreply.github.com>
2018-10-02 20:18:08 +0300

    addedatstic members to the Time class

--- laboratory_work_1/lab_1/second part/lab/main.cpp
+++ laboratory_work_1/lab_1/second part/lab/main.cpp
@@ -5,22 +5,22 @@ class Time {
     int day;
     int month;
     int year;
-    static int defaultDay;
-    static int defaultMonth;
-    static int defaultYear;
-    pair <int, int> time;
-    static pair <int, int> defaultTime;
+    static const int defaultDay = 0;
+    static const int defaultMonth = 0;
+    static const int defaultYear = 0;
+    pair <int, int> time = make_pair(0, 0);
+    const static pair <int, int> defaultTime;
     bool validateSring(string test) {
         if (test.find(":") != std::string::npos) {
             size_t pos = test.find(":");
             string s1 = test.substr(0, pos);
             string s2 = test.substr(pos + 1, std::string::npos - pos - 1);
             bool good = true;
-            for (int i = 0; i < s1.size(); i++) {
+            for (int i = 0; i < (int)s1.size(); i++) {
                 if (i > 1 || s1[i] < '0' || s1[i] > '9') good = false;
             }
             if (!good) return false;
-            for (int i = 0; i < s2.size(); i++) {
+            for (int i = 0; i < (int)s2.size(); i++) {
                 if (i > 1 || s2[i] > '9' || s2[i] < '0') good = false;
             }
             if (!good) return false;
@@ -36,18 +36,18 @@ public:
         this->month = month;
         this->year = year;
     }
-    ~Time() {}
-    void setDefaultTime(pair <int, int> time, int day, int month, int year) {
-        defaultTime.first = time.first;
-        defaultTime.second = time.second;
-        defaultDay = day;
-        defaultMonth = month;
-        defaultYear = year;
+    Time(string time, int day = defaultDay, int month = defaultMonth, int year = defaultYear) {
+        if (!validateSring(time)) {
+            cerr << "Bad data\n";
+        }
+        int pos = time.find(":");
+        int first = stoi(time.substr(0, pos));
+        int second = stoi(time.substr(pos + 1, std::string::npos));
+        cout << first << ' ' << second;
     }
+    ~Time() {}
 };
 
 int main() {
-    Time t;
-    t.test("22:22");
     return 0;
 }
